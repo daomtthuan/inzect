9ba07df7a82647f6ad6406e02d22c4674f4650e6
daomtthuan <thuandmt@outlook.com>
2025-05-02 15:26:23 +0700

    Implement resolution context

--- src/constants/_inject.ts
+++ src/constants/_inject.ts
@@ -5,5 +5,5 @@
  */
 export class _InjectConstants {
   /** Inject Class arguments. */
-  public static readonly injectClassOptions = Symbol('InjectClass.options');
+  public static readonly injectClassOptions: symbol = Symbol('InjectClass.options');
 }
